56cb07e837473c876592ca08384430f3cf8572de
titovskyi <titovskyi@gmail.com>
2017-04-04 16:58:41 +0300

    добавил фалы что б работали сладеры

---
+++ code/js/slider_banner.js
@@ -0,0 +1,22 @@
+  $('.slider_banner').slick({
+  	slidesToScroll: 1,
+  	autoplay: true,
+  	autoplaySpeed: 1000,
+  	arrows: false,
+  	infinite: true,
+  	speed: 150,
+  	fade: true,
+  	cssEase: 'linear'
+  });
+  
+  $('.sl').slick({
+  	slidesToScroll: 1,
+/*  	autoplay: true,*/
+/*  	autoplaySpeed: 1000,*/
+  	arrows: false,
+  	dots: true,
+  	infinite: true,
+  	speed: 150,
+  	fade: true,
+  	cssEase: 'linear'
+  });
