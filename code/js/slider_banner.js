3e21d37ed54de2dc77fd7f56308af57dcfd44dbc
titovskyi <titovskyi@gmail.com>
2017-04-08 17:38:47 +0300

    немного доделан сладер процесс создания. сделан слайдер с партнерами.

--- code/js/slider_banner.js
+++ code/js/slider_banner.js
@@ -1,7 +1,7 @@
   $('.slider_banner').slick({
   	slidesToScroll: 1,
   	autoplay: true,
-  	autoplaySpeed: 1000,
+  	autoplaySpeed: 3000,
   	arrows: false,
   	infinite: true,
   	speed: 150,
@@ -11,8 +11,8 @@
   
   $('.sl').slick({
   	slidesToScroll: 1,
-/*  	autoplay: true,*/
-/*  	autoplaySpeed: 1000,*/
+  	autoplay: false,
+  	autoplaySpeed: 3000,
   	arrows: false,
   	dots: true,
   	infinite: true,
@@ -20,3 +20,74 @@
   	fade: true,
   	cssEase: 'linear'
   });
+
+  $('.sl_partners').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 820,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
+
+/*$('.sl_partners').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});*/
